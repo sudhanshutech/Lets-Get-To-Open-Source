e4b6f600ad4c6cbf1ed042788a3ce0403b94e724
sudhanshu dasgupta <dasguptashivam23@gmail.com>
2024-01-21 12:14:47 +0530

    fix naming, add another tutuorial of defer
    Signed-off-by: sudhanshu dasgupta <dasguptashivam23@gmail.com>

---
+++ Lets-Learn-Golang/17defer/go.mod
@@ -0,0 +1,3 @@
+module 17defer
+
+go 1.21.0
---
+++ Lets-Learn-Golang/17defer/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	defer fmt.Println("world")
+	println("hello")
+	myDefer()
+}
+
+func myDefer() {
+	for i := 0; i < 5; i++ {
+		defer fmt.Println(i)
+	}
+}
+
+// Description:
+// A defer statement defers the execution of a function until the surrounding function returns.
+// Defer is used to ensure that a function call is performed later in a program’s execution,
+// usually for purposes of cleanup. defer is often used where e.g. ensure and finally would be used in other languages.
+// LIFO order (last-in-first-out): the last deferred function is executed first.
